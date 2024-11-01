6241d373bce73c02f4dff1eddb6acdf6c511b2cb
sCoruja <strigocoruja@yandex.ru>
2024-11-01 17:39:28 +0300

    fix main page links

--- packages/client/src/pages/Main/index.tsx
+++ packages/client/src/pages/Main/index.tsx
@@ -31,7 +31,7 @@ export const MainPage = () => {
                     <Typography variant="body1">
                         {mainPageData.leaderboard}
                     </Typography>
-                    <Link to={"/game"} className={styles.button}>
+                    <Link to={"/leaderboard"} className={styles.button}>
                         <Typography variant="body1">
                             Go to leaderboard
                         </Typography>
@@ -51,7 +51,7 @@ export const MainPage = () => {
                     <Typography variant="body1">
                         {mainPageData.forum}
                     </Typography>
-                    <Link to={"/game"} className={styles.button}>
+                    <Link to={"/forum"} className={styles.button}>
                         <Typography variant="body1">Go to forum</Typography>
                     </Link>
                 </Stack>
